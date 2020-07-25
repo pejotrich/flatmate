6d8f49336dd277e04fd3ab2105a6f7af2387e3b1
Johanna Petrich <pejotrich@gmail.com>
2020-07-25 17:55:39 +0200

    added request controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,10 +6,30 @@ class RequestsController < ApplicationController
 
   def create
     @request = Request.new(request_params)
+    @request.user_id = current_user.id 
     @request.save
     redirect_to request_path(@request)
   end
 
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def destroy
+    @request = Request.find(params[:id])
+    @request.destroy
+    redirect_to requests_path
+  end
+
+  def edit
+    @request = Request.find(params[:id])
+  end
+
+  def update
+    @request = Request.find(params[:id])
+    @request.update(city: params[:request][:city], budget: params[:request][:budget], radius: params[:request][:radius], no_of_flat_mates: params[:request][:no_of_flat_mates], size: params[:request][:size], move_in_date_earliest: params[:request][:move_in_date_earliest], move_in_date_latest: params[:request][:move_in_date_latest], , address:, radius: params[:request][:radius] params[:request][:address], privacy_level: params[:request][:privacy_level])
+    redirect_to request_path(@request)    
+  end
 
   private
 
