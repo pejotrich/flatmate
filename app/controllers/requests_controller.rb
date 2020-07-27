93e3eda54f3461ed7e45d3ec6c22c499ddadc69b
Adrian Schultze <adrianschultze@gmail.com>
2020-07-27 19:47:00 +0200

    added pundit policy and request policy

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,6 +2,7 @@ class RequestsController < ApplicationController
 
   def new
     @request = Request.new
+    authorize @request
   end
 
   def create
@@ -9,26 +10,31 @@ class RequestsController < ApplicationController
     @request.user_id = current_user.id
     @request.save
     redirect_to request_path(@request)
+    authorize @request
   end
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    authorize @request
   end
 
   def destroy
     @request = Request.find(params[:id])
     @request.destroy
     redirect_to requests_path
+    authorize @request
   end
 
   def edit
     @request = Request.find(params[:id])
+    authorize @request
   end
 
   def update
     @request = Request.find(params[:id])
-    @request.update(city: params[:request][:city], budget: params[:request][:budget], radius: params[:request][:radius], no_of_flat_mates: params[:request][:no_of_flat_mates], size: params[:request][:size], move_in_date_earliest: params[:request][:move_in_date_earliest], move_in_date_latest: params[:request][:move_in_date_latest], address: params[:request][:address], radius: params[:request][:radius], privacy_level: params[:request][:privacy_level])
+    @request.update(city: params[:request][:city], budget: params[:request][:budget], no_of_flat_mates: params[:request][:no_of_flat_mates], size: params[:request][:size], move_in_date_earliest: params[:request][:move_in_date_earliest], move_in_date_latest: params[:request][:move_in_date_latest], address: params[:request][:address], radius: params[:request][:radius], privacy_level: params[:request][:privacy_level])
     redirect_to request_path(@request)
+    authorize @request
   end
 
   private
