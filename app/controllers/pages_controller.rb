d750e74efba42433b61dd675e93aa4506a8c9afc
Maria Fernandez <mariapop81@gmail.com>
2020-08-06 12:57:45 +0200

    dashboard in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,13 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @my_requests = Request.where(user_id: current_user.id)
+    @my_offers = []
+    @my_requests.each do |request|
+      @my_offers  << request.offers
+    end
+
+  end
 end
