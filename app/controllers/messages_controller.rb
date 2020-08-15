e634d2a49bb20e4be598c1181c6c00e7c8390a50
Adrian Schultze <adrianschultze@gmail.com>
2020-08-15 16:30:50 +0200

    edited show view more

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,27 @@
 class MessagesController < ApplicationController
   def create
+    puts params
     @offer = Offer.find(params[:offer_id])
+    @offer.status = params[:status]
+    @offer.save
+    @request = @offer.request
     @message = Message.new(message_params)
     @message.offer = @offer
     @message.sender = current_user
     authorize @message
     if @message.save
+      if @offer.status == "declined"
+        puts "THIS IS WORKING"
+        request_offer_decline_path(@request, @offer)
+        redirect_to request_offer_path(@request, @offer)
+      elsif @offer.status == "accepted"
+        request_offer_accept_path(@request, @offer)
+        redirect_to request_offer_path(@request, @offer)
+      end
       ChatroomChannel.broadcast_to(
       @offer,
       render_to_string(partial: "message", locals: { message: @message })
       )
-      redirect_to offer_show1_path(@offer, anchor: "message-#{@message.id}")
     else
       render "offers/show"
     end
